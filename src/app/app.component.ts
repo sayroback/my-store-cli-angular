864f63f748ee83bd1a341166ce7dd9612d938ad0
Juan Manuel Calderon Robles <sayroback@gmail.com>
2023-02-18 15:38:17 -0600

    Eventos y atributos HTML

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-store-cli-angular';
+	name = 'Juan';
+	age = 23;
+	img = 'https://source.unsplash.com/random';
+	btnDisabled = true;
+	person = {
+		name: 'Juan',
+		age: 23,
+		img: 'https://source.unsplash.com/random',
+	};
+	scroll = 0;
+
+	toggleButton() {
+		this.btnDisabled = !this.btnDisabled;
+	}
+
+	increaseAge() {
+		this.person.age++;
+	}
+
+	onScroll(event: Event) {
+		const element = event.target as HTMLElement;
+		this.scroll = element.scrollTop;
+	}
+
+	onKeyUp(event: Event) {
+		const element = event.target as HTMLInputElement;
+		this.person.name = element.value;
+	}
 }
